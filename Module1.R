4b375c7e988d47b57933e23707993f9f41e135ec
KDibeley <kdibeley@Tertios-MacBook-Pro.local>
2022-06-29 21:56:18 -0700

    Includes a plot of the distribution of the number of listed household members for the entire sample.

--- Module1.R
+++ Module1.R
@@ -1,12 +1,32 @@
 # New Script 
 library(tidyverse)
 library(haven)
+library(vtable)
+library(dplyr)
+library(ggplot2)
 
-nfhs <- read_dta("IAHR52FL.dta")
+# loads the given data as nfhs
+nfhs <- read_dta("IAHR52FL.dta") 
 
-new_def <- nfhs %>%
-select(hhid:shstruc)
+# Selects the data between hhid:shstruc
+new_def <- nfhs %>% 
+  select(hhid:shstruc)
 
+# Plots the distribution of the number of listed household members for the entire sample 
+ggplot(data = nfhs, mapping = aes(x = hv009)) +  
+  geom_histogram(binwidth = 1) + 
+  xlab("Number of Listed Household Members")
+
+#5 Create a new data frame that contains only urban households (the variable is "hv025").
+
+new_data_urban_household <- nfhs %>%
+  select(hv025)
+
+box_plot <- ggplot(data = new_data_urban_household, mapping = aes(x = is.factor(hv025))) + 
+  geom_boxplot()
+
+
+#6 
 
 
 
